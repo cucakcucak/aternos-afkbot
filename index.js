b671ff570058572392dc4f02b30fd20d9f718c0e
cucakcucak <minhtienhahahavui@gmail.com>
2025-06-17 18:38:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,63 +1,19 @@
 const mineflayer = require('mineflayer');
 const fs = require('fs');
-let rawdata = fs.readFileSync('config.json');
-let data = JSON.parse(rawdata);
 
-var lasttime = -1;
-var moving = 0;
-var connected = 0;
-var actions = [ 'forward', 'back', 'left', 'right'];
-var lastaction;
-var pi = 3.14159;
-var moveinterval = 2; // 2 second movement interval
-var maxrandom = 5; // 0-5 seconds added to movement interval (randomly)
+const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 
-var host = data["host"];          // ✔ ĐÚNG
-var port = data["port"];          // ✔ ĐÚNG
-var username = data["username"];  // ✔ ĐÚNG
-
-var bot = mineflayer.createBot({
-  host: host,
-  port: port,
-  username: username
+const bot = mineflayer.createBot({
+    host: config.host,
+    port: config.port,
+    username: config.username,
+    version: '1.21.4' // Thêm dòng này cho đúng phiên bản server
 });
 
-function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
-}
-
-bot.on('login', function(){
-	console.log("Logged In");
+bot.on('chat', (username, message) => {
+    if (username === bot.username) return;
+    bot.chat('Tôi đang online để giữ slot!');
 });
 
-bot.on('time', function() {
-    if (connected < 1) {
-        return;
-    }
-    if (lasttime < 0) {
-        lasttime = bot.time.age;
-    } else {
-        var randomadd = Math.random() * maxrandom * 20;
-        var interval = moveinterval * 20 + randomadd;
-        if (bot.time.age - lasttime > interval) {
-            if (moving == 1) {
-                bot.setControlState(lastaction, false);
-                moving = 0;
-                lasttime = bot.time.age;
-            } else {
-                var yaw = Math.random() * pi - (0.5 * pi);
-                var pitch = Math.random() * pi - (0.5 * pi);
-                bot.look(yaw, pitch, false);
-                lastaction = actions[Math.floor(Math.random() * actions.length)];
-                bot.setControlState(lastaction, true);
-                moving = 1;
-                lasttime = bot.time.age;
-                bot.activateItem();
-            }
-        }
-    }
-});
-
-bot.on('spawn', function() {
-    connected = 1;
-});
+bot.on('error', err => console.log('Error:', err));
+bot.on('end', () => console.log('Bot bị ngắt kết nối, đang khởi động lại...'));
